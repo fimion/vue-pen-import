668851284019bfab01f5fd73110516fa6a07d616
fimion <fimion@gmail.com>
2020-12-14 22:07:03 -0500

    imports?

--- functions/import.js
+++ functions/import.js
@@ -1,10 +1,24 @@
+/**
+ * @typedef {object} LambdaEvent
+ * @property {object} queryStringParameters
+ */
+
+/**
+ *
+ * @param {LambdaEvent} event
+ * @param context
+ * @returns {Promise<{headers: {contentType: string}, body: string, statusCode: number}>}
+ */
 module.exports.handler = async function(event, context) {
+  const script = event.queryStringParameters.script
+  const pen = `https://codepen.io/${script}.js`;
   // your server-side functionality
   return {
     statusCode: 200,
     headers:{contentType:"application/javascript"},
-    body:`
-      export default {};
-    `,
+    body:`window.self = window.self || {};
+import '${pen}';
+const component = self.CodePenVueComponent
+export default component;`,
   };
 }
